2e8cd582287bd737ffd156adda19f0221f45f490
Tayler Schanks <taylerschanks2@gmail.com>
2023-05-01 19:39:01 -0500

    lab objective

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true
+      }
+    }
+  }
+  return false
 }
-
 /* 
   Write the Big O time complexity of your function here
+  This is Quadratic time because we need to check each number in the array to each other to see if they add to the target number
 */
 
 /* 
   Add your pseudocode here
+
 */
 
 /*
   Add written explanation of your solution here
+  look at each number in the array
+  look at the rest of the numbers
+  check if the two numbers add up to the target number
+  if yes return true
+  else return false
 */
 
 // You can run `node index.js` to view these console logs
